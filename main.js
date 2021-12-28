cb1eb1bed4406055c1c388630445e283bbd2fb69
Sardius <0xsardius@gmail.com>
2021-12-28 01:30:12 -0500

    Minimal example lazy minter

--- main.js
+++ main.js
@@ -2,10 +2,11 @@
 const serverUrl = "https://byuk2ifryfqg.usemoralis.com:2053/server";
 const appId = "Ny4quiyZ5FlQiT62F38CVnrUfBxnZkhPAHLIhSse";
 Moralis.start({ serverUrl, appId });
+let user;
 
 /** Add from here down */
 async function login() {
-  let user = Moralis.User.current();
+  user = Moralis.User.current();
   if (!user) {
    try {
       user = await Moralis.authenticate({ signingMessage: "Hello World!" })
@@ -15,6 +16,7 @@ async function login() {
    }
   }
   else {
+    Moralis.enableWeb3();
      initApp() 
   }
 }
@@ -28,14 +30,40 @@ async function submit() {
     // Get image data
     const input = document.querySelector("#input_image");
     let data = input.files[0];
+    //Upload image to iPFS
     const imageFile = new Moralis.File(data.name, data);
     await imageFile.saveIPFS();
     let imageHash = imageFile.hash();
     console.log(imageHash);
-    console.log(imageFile.ipfs());
-    // Upload image to IPFS
-    // Create metadata with image hash and data from
+    // Create metadata with image hash and data
+    let metadata = {
+        name: document.querySelector("#input_name").value,
+        description: document.querySelector("#input_description").value,
+        image: "/ipfs/" + imageHash
+    }
+    // Upload metadata to iPFS
+    const jsonFile = new Moralis.File("metadata.json", {base64: btoa(JSON.stringify(metadata))})
+    await jsonFile.saveIPFS();
+    let metadataHash = jsonFile.hash();
+    console.log(metadataHash);
     // Upload to Rarible (plugin)
+    await Moralis.Plugins.rarible.lazyMint({
+        chain: 'rinkeby',
+        userAddress: user.get('ethAddress'),
+        tokenType: "ERC721",
+        tokenUri: '/ipfs/' + metadataHash,
+        royaltiesAmound: 47
+    })
+    console.log(res);
+    let token_address = res.data.result.token_address;
+    let token_id = res.data.result.token_address;
+    let url = `https://rinkeby.rarible.com/token/${token_address}:${token_id}`
+    document.querySelector("#success_message").innerHTML = 
+        `NFT Minted. <a target="_blank" href="${url}">View NFT</a>`
+    document.querySelector("#success_message").style.display = "block";
+    setTimeout(() => {
+        document.querySelector("#success_message").style.display = "none";
+    }, 5000)
 }
 
 login();
